4b02184868e27c78eac4ffd9b83701bc2faa1af6
bogdanz <bogdanz@gmail.com>
2018-10-04 12:05:18 +0300

    add babel plugin transform-class-properties, preset es2015, remove stage-2 preset

--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
 // @flow
 
-const createPoint = (x: number, y: number) => ({
-  x, y,
+type WorkType = 'slow' | 'middle' | 'hard'
+
+type WorkProgress = {
+  done: boolean,
+  percent: number,
+  workType: WorkType,
+}
+
+type Animal = {
+  name: string,
+  age: number,
+  speak(): void,
+  reportWorkProgress(workType: WorkType): WorkProgress,
+}
+
+const createAnimal = (name: string, age: number): Animal => ({
+  name,
+  age,
+  speak() {
+    console.log(name, 'speaking')
+  },
+  reportWorkProgress(workType) {
+    return {
+      done: false,
+      percent: 20,
+      workType,
+    }
+  },
 })
 
-const p = createPoint(10, 5)
+const humanizeWorkProgress = (workProgress: WorkProgress) => {
+  console.log(workProgress.done ? 'Work done' : 'Work does not done')
+  console.log('with percent:', workProgress.percent)
+  console.log('and work type:', workProgress.workType)
+}
 
+const animal = createAnimal('Bear', 10)
+const workProgress = animal.reportWorkProgress('hard')
 
+animal.speak()
+humanizeWorkProgress(workProgress)
+
+class Some {
+  handleClick = () => {
+
+  }
+}
+
+const addNums = (a: number, b :number): number => a + b
+
+console.log(addNums(10, 5))
